f7ba2b0cea6be082b894828def8c4774b21ba28e
Federico González <alambratore@hotmail.com>
2025-01-17 12:03:20 -0300

    New models

--- express-app/src/routes/index.ts
+++ express-app/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import ingredients from './ingredient.routes';
+import pizzas from './pizza.routes';
 
 const router = Router();
 
 router.use('/ingredients', ingredients);
+router.use('/pizzas', pizzas);
 
 export default router;
