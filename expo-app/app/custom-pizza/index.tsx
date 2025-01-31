c8daced852790da513b1f388311dd867da2362c8
Federico González <alambratore@hotmail.com>
2025-01-31 20:14:27 -0300

    Info de coomo terminar la pantalla CustomPizza

--- expo-app/app/custom-pizza/index.tsx
+++ expo-app/app/custom-pizza/index.tsx
@@ -8,11 +8,54 @@ const CustomPizzaScreen = () => {
   const router = useRouter();
 
   // Estado para tamaños y precios
-  const [sizes, setSizes] = useState<{ name: string; basePrice: number }[]>([]);
+  const [sizes, setSizes] = useState<{ name: string; basePrice: number; }[]>([]);
+  /*  */
+  const [ingredients, setIngredients] = useState<{
+    id: number,
+    name: string,
+    extraCost: number;
+  }[]>([]);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
   const [selectedSize, setSelectedSize] = useState<string>("");
   const [selectedPrice, setSelectedPrice] = useState<number | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+
+  const onSubmit = async () => {
+    // Armar la pizza personalizada
+
+
+  };
+  /* 
+      chica - muzzarella - piña
+      crearla en la base de datos (hacer post a la api con cierta in formación)
+  
+      POST http://localhost:3000/api/custom-pizzas
+      {
+      "customPizza": {
+          "sizeId": 1,
+          "ingredients": [
+              {
+                  "id": 1,
+                  "name": "Salsa de tomate",
+                  "extraCost": 0.5
+              },
+              {
+                  "id": 30,
+                  "name": "Queso provolone",
+                  "extraCost": 1.8
+              },
+              {
+                  "id": 8,
+                  "name": "Aceitunas verdes",
+                  "extraCost": 0.8
+              }
+          ]
+      }
+    }
+      agregarla al carrito
+  */
 
   useEffect(() => {
     const fetchSizes = async () => {
